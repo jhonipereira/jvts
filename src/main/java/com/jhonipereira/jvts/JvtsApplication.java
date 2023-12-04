024c01ab0f2e30c3e92a6ea5528ea0ce934bbc2a
Jhoni <113802785+jhonipereira@users.noreply.github.com>
2023-12-04 11:34:52 +0000

    initial commit

---
+++ src/main/java/com/jhonipereira/jvts/JvtsApplication.java
@@ -0,0 +1,13 @@
+package com.jhonipereira.jvts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhonipereira/jvts/JvtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonipereira.jvts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
